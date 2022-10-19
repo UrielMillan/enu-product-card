299748ac0f70bcad2445f10c6dd506dda56734b6
Erick <uriel.noriegam@gmail.com>
2022-10-19 11:58:20 -0600

    listo para subir a npm

---
+++ test/data/products.ts
@@ -0,0 +1,11 @@
+export const product1 = {
+    id: '131',
+    title: 'coffe mug sin imagen',
+}
+
+
+export const product2 = {
+    id: '132',
+    title: 'coffe mug con imagen',
+    img: './coffee-mug.png'
+}
